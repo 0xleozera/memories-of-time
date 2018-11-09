504447dc4f008604fd2c8bdb78cf6436cf721672
leonardorpr <leonardo.rocha@ulbra.inf.br>
2018-11-09 03:47:27 -0200

    chore(*) syntax precompile

--- app/assets/javascripts/memories.js
+++ app/assets/javascripts/memories.js
@@ -1,17 +1,17 @@
 function initMap() {
-  const city = document.getElementById('memory_city');
+  var city = document.getElementById('memory_city');
 
-  const autocomplete = new google.maps.places.Autocomplete(city);
+  var autocomplete = new google.maps.places.Autocomplete(city);
 
-  const infowindow = new google.maps.InfoWindow();
-  const infowindowContent = document.getElementById('infowindow-content');
+  var infowindow = new google.maps.InfoWindow();
+  var infowindowContent = document.getElementById('infowindow-content');
 
   infowindow.setContent(infowindowContent);
 
-  autocomplete.addListener('place_changed', () => {
+  autocomplete.addListener('place_changed', function() {
     infowindow.close();
 
-    const place = autocomplete.getPlace();
+    var place = autocomplete.getPlace();
 
     if (!place.geometry) {
       return;
@@ -25,7 +25,7 @@ function initMap() {
 // TODO - only do it when it's free
 
 // function fetchWeatherAndTemperature(day, lat, long) {
-//   const url = `https://api.openweathermap.org/data/2.5/history/city?lat=${lat}&lon=${long}&type=hour&start=${day}&end=${day}&appid=${3f311ddae225b418964aa7b3f45f1508}`
+//   var url = `https://api.openweathermap.org/data/2.5/history/city?lat=${lat}&lon=${long}&type=hour&start=${day}&end=${day}&appid=${3f311ddae225b418964aa7b3f45f1508}`
 
 //   fetch(url)
 //     .then(response => console.log(response.json()))
