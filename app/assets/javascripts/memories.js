9c8409349b32eebf490b21910c050e071ec47cb3
leonardorpr <leonardo.rocha@ulbra.inf.br>
2018-11-09 00:14:45 -0200

    fix (memories) set data before change city

--- app/assets/javascripts/memories.js
+++ app/assets/javascripts/memories.js
@@ -1,10 +1,7 @@
-
 function initMap() {
-  const input = document.getElementById('memory_city');
-  // const lat = document.getElementById('memory_lat').value;
-  // const lng = document.getElementById('memory_long').value;
+  const city = document.getElementById('memory_city');
 
-  const autocomplete = new google.maps.places.Autocomplete(input);
+  const autocomplete = new google.maps.places.Autocomplete(city);
 
   const infowindow = new google.maps.InfoWindow();
   const infowindowContent = document.getElementById('infowindow-content');
@@ -15,10 +12,21 @@ function initMap() {
     infowindow.close();
 
     const place = autocomplete.getPlace();
-    //console.log(place.geometry.location.lat());
 
     if (!place.geometry) {
       return;
     }
+
+    document.getElementById('memory_lat').value = place.geometry.location.lat();
+    document.getElementById('memory_long').value = place.geometry.location.lng();
   });
 }
+
+// TODO - only do it when it's free
+
+// function fetchWeatherAndTemperature(day, lat, long) {
+//   const url = `https://api.openweathermap.org/data/2.5/history/city?lat=${lat}&lon=${long}&type=hour&start=${day}&end=${day}&appid=${3f311ddae225b418964aa7b3f45f1508}`
+
+//   fetch(url)
+//     .then(response => console.log(response.json()))
+// }
